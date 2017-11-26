15eeb525112f811fe9fd8b4ceb476035d073f850
skkriss <krystian_wos77@wp.pl>
2017-11-26 20:03:41 +0100

    koncowy2

--- BubbleSort/lib/bubble_sort.rb
+++ BubbleSort/lib/bubble_sort.rb
@@ -1,14 +1,24 @@
-# Bubble sort class
 def bubble_sort(array)
-  done = false
-  until done
-    done = true
-    (array.length - 1).times do |i|
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]
-        done = false
+  if array.empty?
+    return raise 'Tablica jest pusta'
+  else
+    array_ = Array.new
+
+    for j in 0...array.length
+      for i in 1...array.length
+        if array[i] < array[i-1]
+          temp = array[i]
+          array[i] = array[i-1]
+          array[i-1] = temp
+          i = i + 1
+        else
+          i = i + 1
+        end
       end
+      array_ << array.last
+      array.delete_at(array.length-1)
+      j = j + 1
     end
   end
-  array
+  return array_.reverse
 end
--- BubbleSort/spec/bubble_sort_spec.rb
+++ BubbleSort/spec/bubble_sort_spec.rb
@@ -1,36 +1,16 @@
-require_relative '../lib/bubble_sort'
-require 'rspec'
+require_relative '../lib/bubblesort.rb'
 
-SimpleCov.start
 
-describe '.bubble_sort' do
-  context 'when get positive integers' do
-    array = [1, 2, 16, 3, 8]
-    it { expect(bubble_sort(array)).eql? ([1, 2, 3, 8, 16]) }
+describe 'Bubble Sort' do
+
+  it 'Not error'  do
+    expect { bubble_sort([2,5]) }.not_to raise_error
   end
-  context 'when get negative integers' do
-    array = [-1, -2, -16, -3, -8]
-    it { expect(bubble_sort(array)).eql? ([-16, -8, -3, -2, -1]) }
+  it 'Equal' do
+    expect(bubble_sort([11,16,1,0])).to eq([0,1,11,16])
   end
-  context 'when get positive floats' do
-    array = [1.1, 2.2, 1.0, 2.5, 1.05]
-    it { expect(bubble_sort(array)).eql? ([1.0, 1.05, 1.1, 2.2, 2.5]) }
+  it 'Not equal' do
+    expect(bubble_sort([3,6,1])).not_to eq([3,1,6])
   end
-  context 'when get negative floats' do
-    array = [-1.1, -2.2, -1.0, -2.5, -1.05]
-    it { expect(bubble_sort(array)).eql? ([-2.5, -2.2, -1.1, -1.05, -1.0]) }
-  end
-  context 'when get same numbers' do
-    before = [3, 3, 3, 3, 3, 3]
-    after = before
-    it { expect(bubble_sort(after)).eql? before }
-    before = [1, 2, 3, 4, 5, 6, 7, 8]
-    after = before
-    it { expect(bubble_sort(after)).eql? before }
-  end
-  context 'after sort' do
-    array = [8, 7, 5, 5, 1, 0, 9]
-    array = bubble_sort(array)
-    it { expect(array[0]).eql? 0 and expect(array[6]).eql? 9 }
-  end
-end
+
+end	
